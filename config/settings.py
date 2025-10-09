6b39491f88fd9f151e0be9678183e6e38818193d
Rodolfo_Gomez <ro.gohe08@gmail.com>
2025-10-08 22:06:45 -0300

    Corrección de estilos login. Queda pendiente la persistencia del inicio de sesion

--- config/settings.py
+++ config/settings.py
@@ -130,4 +130,5 @@ STATICFILES_DIRS=[
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'dashboard'    
+LOGIN_REDIRECT_URL = 'dashboard' 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False 
